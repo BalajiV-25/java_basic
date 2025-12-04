7a97e1441b6392310a72247af4b21768c4c58f99
BalajiV-25 <149779365+BalajiV-25@users.noreply.github.com>
2025-12-04 11:42:00 +0530

    Create Bitwise_left_shift.java

---
+++ Bitwise_left_shift.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int m = obj.nextInt();
+        int res=n<<m;
+        System.out.println(res);
+    }
+}
