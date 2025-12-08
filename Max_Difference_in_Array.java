01b10d1a7927459af5f75749739d80a01579522f
BalajiV-25 <149779365+BalajiV-25@users.noreply.github.com>
2025-12-08 23:38:35 +0530

    Create Max_Difference_in_Array.java

---
+++ Max_Difference_in_Array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=obj.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[n-1]-arr[0]);
+        
+    }
+}
